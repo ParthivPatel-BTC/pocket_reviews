6237f1806a7b59d2886998f5db680f0eaaf525f0
parthivpatel <parthiv.patel@botreeconsulting.com>
2013-10-29 14:49:28 +0530

    change header links after Client / Admin logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,21 @@
 class ApplicationController < ActionController::Base
   #protect_from_forgery
   #helper_method :current_user
-   #before_filter :configure_permitted_parameters, if: :devise_controller?
+  #before_filter :configure_permitted_parameters, if: :devise_controller?
+
   def after_sign_in_path_for(resource)
     @user = current_user
     @user_role = UserRole.find_by_user_id(@user.id)
-
     if @user_role == nil
       admins_path
     else
       client_dashboard_path
     end
+
   end
 
   def after_sign_up_path_for(resource)
     users_index_path
   end
+
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  def is_user?
+    @user = current_user
+    return nil unless @user
+    @user_role = UserRole.find_by_user_id(@user.id)
+  end
+
+  def is_current_user?
+    @user = current_user
+  end
 end
