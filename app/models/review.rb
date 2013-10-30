6c9bc41df01219dbf252bca01c205408fc004afa
parthivpatel <parthiv.patel@botreeconsulting.com>
2013-10-30 17:56:58 +0530

    Assign posted_status and posted_on for accessible in review.rb

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,7 @@
 class Review < ActiveRecord::Base
   attr_accessible :comment, :rating, :submitted_on
-  attr_accessible :client_id, :customer_id
+  attr_accessible :client_id, :customer_id, :posted_status, :posted_on
+
 
   belongs_to :client
   belongs_to :customer
