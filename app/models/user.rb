d80c0b3f24022b1c5b98bac4daedeed05329311d
parthivpatel <parthiv.patel@botreeconsulting.com>
2013-10-30 23:06:57 +0530

    Remove cancel button from change password page

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,8 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name, :avatar, :encrypted_password, :username
   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+
+  validates_presence_of :company_logo
   # attr_accessible :title, :body
 
   #validates_presence_of :email, :password, :password_confirmation
